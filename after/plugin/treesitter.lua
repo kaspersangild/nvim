0cf5739f813f08bd8c7508a989a06d6ef062d20b
Kasper Sangild Christensen <kaspersangild@github.com>
2023-01-06 14:35:41 +0100

    Markdown install

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "help", "python", "lua", "latex", "bibtex", "vim"},
+  ensure_installed = { "help", "python", "lua", "latex", "bibtex", "vim", "markdown"},
   sync_install = false,
   auto_install = true,
   highlight = {
